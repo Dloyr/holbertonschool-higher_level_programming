00d8e968e8dbe9e43f07abc6a55a1475905a69da
Dloyr <d.loyer59870@gmail.com>
2024-01-22 13:12:50 +0100

    print alphabet in lowercase except e and q

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# print the alphabet in lowercase execpt e and q
+
+for letter in range(97, 122):
+    if (letter == 101 or letter == 113):
+        continue
+
+    print(chr(letter), end="")
