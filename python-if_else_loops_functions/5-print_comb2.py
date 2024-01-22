139e82661e5792ee839506f052d9a14f796f02e2
Dloyr <d.loyer59870@gmail.com>
2024-01-22 13:42:04 +0100

    print numbers from 0 to 99

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# print numbers from 0 to 99
+
+for numbers in range(0, 100):
+    if (numbers < 99):
+        print(f"{numbers:02d}", end=", ")
+    else:
+        print(f"{numbers}", end="\n")
