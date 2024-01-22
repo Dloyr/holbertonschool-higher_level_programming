707c3354643c6722f9983eff8460307d839768c8
Dloyr <d.loyer59870@gmail.com>
2024-01-22 12:55:17 +0100

    print a different message if enddigit is > 5 , < 6 or == 0

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+enddigit = abs(number) % 10
+
+# print a different message if enddigit is > 5 , < 6 or == 0
+if (enddigit > 5):
+    print(f"Last digit of {number} is {enddigit} and is greater than 5")
+elif (enddigit != 0):
+    print(f"Last digit of {number} is {enddigit} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number} is {enddigit} and is 0")
