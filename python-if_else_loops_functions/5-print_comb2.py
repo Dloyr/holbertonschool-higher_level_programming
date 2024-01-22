3f353c707e73a18ac2be56145a8be6d6aaa36c04
Dloyr <d.loyer59870@gmail.com>
2024-01-22 22:04:49 +0100

    create a variable for store the separator and change it for a "\n" if numbers is equal to 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # print numbers from 0 to 99
 
+separator = ", "
 for numbers in range(0, 100):
-    if (numbers < 99):
-        print("{:02}".format(numbers), end=", ")
-    else:
-        print("{:02}".format(numbers))
+    if (numbers == 99):
+        separator = "\n"
+    print("{:02}".format(numbers), end=separator)
