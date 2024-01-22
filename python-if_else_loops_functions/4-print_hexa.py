a9dda28cf741a8f62435d0461bbf6c0573fe5ad9
Dloyr <d.loyer59870@gmail.com>
2024-01-22 21:09:08 +0100

    use the .format for print for not store numbers or strings  in a variable

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -2,4 +2,4 @@
 # print numbers from 0 to 98 in decimal and hexadecimal
 
 for hexadecimal in range(0, 99):
-    print(str(hexadecimal) + " = " + hex(hexadecimal))
+    print("{} = {}".format(str(hexadecimal), hex(hexadecimal)))
