a29b6f824d954dbe815ba9305862040c14b7c1bc
Dloyr <d.loyer59870@gmail.com>
2024-01-22 20:51:35 +0100

    modify the end of the loop for to 123

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # print the alphabet in lowercase execpt e and q
 
-for letter in range(97, 122):
+for letter in range(97, 123):
     if (letter == 101 or letter == 113):
         continue
 
