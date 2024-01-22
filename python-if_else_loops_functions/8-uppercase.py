2e4c4940dd5ca0d66c26d52dae380945dc0993e3
Dloyr <d.loyer59870@gmail.com>
2024-01-22 19:35:45 +0100

    print a string in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# print a string in uppercase
+
+def uppercase(str):
+    upper_string = ""
+
+    for letter in str:
+        if (97 <= ord(letter) <= 122):
+            upper_string += chr(ord(letter) - 32)
+        else:
+            upper_string += letter
+
+    print(upper_string)
