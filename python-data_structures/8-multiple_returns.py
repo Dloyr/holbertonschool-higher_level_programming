8551e26552b6ee57b479407aed37dd0a5b246674
Dloyr <d.loyer59870@gmail.com>
2024-01-30 19:09:05 +0100

    return a tuple with the length of a string and its first character

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# return a tuple with the length of a string and its first character
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0]
+
+    return length, first
