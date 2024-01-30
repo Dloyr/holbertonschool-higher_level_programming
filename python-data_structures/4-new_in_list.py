ff6244989d586f02b2d11d831e337fddd9b54e35
Dloyr <d.loyer59870@gmail.com>
2024-01-30 18:05:09 +0100

    replace an element at a index in a copy of the list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# replace an element at a index in a copy of the list
+def new_in_list(my_list, idx, element):
+    copy_list = ""
+
+    if (idx < 0 or idx > len(my_list)):
+        return my_list[:]
+    else:
+        copy_list = my_list[:]
+        copy_list[idx] = element
+        return copy_list
