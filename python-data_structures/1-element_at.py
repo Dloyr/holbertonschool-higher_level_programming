a40b1c84c43b7434dca3218a550c2f6aab6068df
Dloyr <d.loyer59870@gmail.com>
2024-01-30 17:28:06 +0100

    retrieves an element from a list.

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# retrieves an element from a list
+def element_at(my_list, idx):
+    if (idx < 0):
+        return None
+
+    if (idx > len(my_list)):
+        return None
+
+    return my_list[idx]
