3b323acabc52abc5da58ac1fa1709d54b825bc01
Dloyr <d.loyer59870@gmail.com>
2024-01-31 15:17:48 +0100

    test without conditions of verification

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,9 +2,5 @@
 
 # replaces or adds key/value in a dictionary
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
-
+    a_dictionary[key] = value
     return a_dictionary
