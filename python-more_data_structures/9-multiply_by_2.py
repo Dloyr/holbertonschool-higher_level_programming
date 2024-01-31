68967c610719467d4e8382d2508113d869323250
Dloyr <d.loyer59870@gmail.com>
2024-01-31 15:36:25 +0100

    remove the variable and return the value of the multiplication

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,4 @@
 
 # returns a new dictionary with all values multiplied by 2
 def multiply_by_2(a_dictionary):
-    new_dictionary = {key: value * 2 for key, value in a_dictionary.items()}
-    return new_dictionary
+    return {key: value * 2 for key, value in a_dictionary.items()}
