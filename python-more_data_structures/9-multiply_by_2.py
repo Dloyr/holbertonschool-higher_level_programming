a3aa82d25f4fe85d28efb8432f4835bf8383e8f2
Dloyr <d.loyer59870@gmail.com>
2024-01-31 15:45:34 +0100

    try with a copy of dictionnary and a multiplication in a loop

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,4 +2,9 @@
 
 # returns a new dictionary with all values multiplied by 2
 def multiply_by_2(a_dictionary):
-    return {key: value * 2 for key, value in a_dictionary.items()}
+    new_dictionary = a_dictionary.copy()
+
+    for index in new_dictionary:
+        new_dictionary[index] = new_dictionary[index] * 2
+
+    return (new_dictionary)
