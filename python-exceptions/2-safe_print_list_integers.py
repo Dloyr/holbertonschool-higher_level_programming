1fc9125ad05fa5d76ef930d337b10bb234fbef27
Dloyr <d.loyer59870@gmail.com>
2024-02-01 12:55:12 +0100

    prints x integers elements of a list with a gestion of error case

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# prints x integers elements of a list with a gestion of error case
+def safe_print_list_integers(my_list=[], x=0):
+    number_elements = 0
+    try:
+        for index in range(0, x):
+            if (isinstance(my_list[index], int)):
+                print("{:d}".format(my_list[index]), end="")
+                number_elements += 1
+    except (TypeError):
+        pass
+    return number_elements
