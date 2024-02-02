a2bd434be79a838276496bd31d27efe4d45f9d44
Dloyr <d.loyer59870@gmail.com>
2024-02-02 11:56:04 +0100

    change the code for a good use of index of matrix and remove the whitespace at the end of lines

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,10 @@
 
 # print a list in a list (double entry table) of integers
 def print_matrix_integer(matrix=[[]]):
-    for index_row in matrix:
-        for index_line in index_row:
-            print("{:d}".format(index_line), end=" ")
+    for index_row in range(len(matrix)):
+        for index_column in range(len(matrix[index_row])):
+            print("{:d}".format(matrix[index_row][index_column]), end="")
+            if index_column != (len(matrix[index_row]) - 1):
+                print(" ", end="")
+
         print()
