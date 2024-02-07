e9f75f56ce01ca54f7037d64945a05df692dc98f
Dloyr <d.loyer59870@gmail.com>
2024-02-07 10:26:23 +0100

    change comment message for have the good number of comments lines

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """
     Adds 2 integers
+    Args:
+        a (int or float) : First value and b (int or float) : Second value
 """
 
 
 def add_integer(a, b=98):
     """
-    Args:
-        a (int or float) : First value
-        b (int or float) : Second value
-
-    Raises:
-        TypeError: If a or b is not in type in or float
-
-    Return:
-        a + b
+    Return: a + b
     """
     if isinstance(a, (int, float)):
         a = int(a)
