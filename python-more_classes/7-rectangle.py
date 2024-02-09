4f36ea24ebab542272ef95f9d68287466e81b95e
Dloyr <d.loyer59870@gmail.com>
2024-02-09 13:03:49 +0100

    use the base of task 6 fix

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -6,59 +6,46 @@
 class Rectangle:
     """rectangle is empty"""
     number_of_instances = 0
-    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """ defines instance of a rectangle with is width and height
-
-        Args:
-            width: width of the rectangle
-            height: height of the rectangle
-        """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        """class method for take the width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """class method for modify the value of width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >=0")
-
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        """class method for take the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """class method for modify the value of height"""
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >=0")
-
-        self.__height = value
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >=0")
+        else:
+            self.__height = value
 
     def area(self):
-        """class method for get the area of the rectangle"""
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter(self):
-        """class method for get the perimeter of the rectangle"""
         if self.width == 0 or self.height == 0:
             return 0
-
-        return self.__width * 2 + self.__height * 2
+        else:
+            return self.width * 2 + self.height * 2
 
     def __str__(self):
         """class method for the representation of strings of rectangle"""
@@ -75,10 +62,10 @@ class Rectangle:
         return rectangle_string[:-1]
 
     def __repr__(self):
-        """class method for the printable representation of an object"""
+        """instance method for the printable representation of an object"""
         return f'Rectangle({self.width}, {self.height})'
 
     def __del__(self):
         """class method for delete the instantate object"""
-        print("Bye rectangle ...")
+        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
