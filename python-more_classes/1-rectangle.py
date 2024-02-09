1d8035853d9471304e7b19950d14d88b72ce3191
Dloyr <d.loyer59870@gmail.com>
2024-02-09 12:06:15 +0100

    change isinstance for type() in verifications

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
     def width(self, value):
         """setter method for modify the value of width"""
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >=0")
@@ -42,7 +42,7 @@ class Rectangle:
     def height(self, value):
         """setter method for modify the value of height"""
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >=0")
