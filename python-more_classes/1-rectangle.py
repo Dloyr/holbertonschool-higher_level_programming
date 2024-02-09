3dd3eb232512efcb738f389821856a90459288a4
Dloyr <d.loyer59870@gmail.com>
2024-02-09 12:00:09 +0100

    change the message error for height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,13 +17,13 @@ class Rectangle:
 
     @property
     def width(self):
-        """class method for take the width of the rectangle"""
+        """property method for take the width of the rectangle"""
 
         return self.__width
 
     @width.setter
     def width(self, value):
-        """class method for modify the value of width"""
+        """setter method for modify the value of width"""
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -34,17 +34,17 @@ class Rectangle:
 
     @property
     def height(self):
-        """class method for take the height of the rectangle"""
+        """property method for take the height of the rectangle"""
 
         return self.__height
 
     @height.setter
     def height(self, value):
-        """class method for modify the value of height"""
+        """setter method for modify the value of height"""
 
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("height must be >=0")
 
         self.__height = value
