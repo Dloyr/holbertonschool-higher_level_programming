1ccf8a17e020f638ee1b09ed1c491a1e5c5a91b3
Dloyr <d.loyer59870@gmail.com>
2024-02-14 14:05:47 +0100

    instance method for calculate the area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class is empty"""
+    pass
+
+    def area(self):
+        """instance method for calculate the area"""
+        raise Exception('area() is not implemented')
