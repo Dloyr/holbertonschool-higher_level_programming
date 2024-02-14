a4f1c08ee4116df633893b67c7e32c6f026a0731
Dloyr <d.loyer59870@gmail.com>
2024-02-14 12:42:24 +0100

    change documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-# return the list of availables attributes and methos of an object
 def lookup(obj):
+    """return the list of availables attributes and methos of an object"""
     return dir(obj)
