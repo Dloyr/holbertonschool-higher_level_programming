8eafbabc3c6fadd884d04fa3277d7bae04ad0e39
Dloyr <d.loyer59870@gmail.com>
2024-02-14 15:00:33 +0100

    instantiation of width and height for inherits class Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class is empty"""
+    pass
+
+    def area(self):
+        """instance method for calculate the area"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """instance method for verify if integer is valid"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """inherited class for make a rectangle"""
+    def __init__(self, width, height):
+        """ define instance of a rectange with his width and height
+
+        Args:
+            width: private attribute width
+            height: private attribute height
+        """
+        super().__init__()
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
