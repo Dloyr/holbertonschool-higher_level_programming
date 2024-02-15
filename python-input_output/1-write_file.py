98f34150839ba07ded50e3bbf75b41d8b9856361
Dloyr <d.loyer59870@gmail.com>
2024-02-15 13:30:12 +0100

    write in a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+write in a file
+"""
+
+
+def write_file(filename="", text=""):
+    """function for write in a file
+
+        Args:
+            filename: the name of the file
+            text: the text in the file
+        Returns:
+            the lenght of the text in file
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
+        file.close()
+    return len(text)
