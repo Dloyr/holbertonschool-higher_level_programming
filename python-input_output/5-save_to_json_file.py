f292e0d20f198b356d1da3dc68c065c13f3503f9
Dloyr <d.loyer59870@gmail.com>
2024-02-18 11:50:57 +0100

    reduce the size of the code

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -15,6 +15,5 @@ def save_to_json_file(my_obj, filename):
         Returns:
             the text file with JSON representation
     """
-    json_object = json.dumps(my_obj)
-    with open(filename, "w") as file:
-        return file.write(json_object)
+    with open(filename, "w", encoding="utf-8") as file:
+        return json.dump(my_obj, file)
