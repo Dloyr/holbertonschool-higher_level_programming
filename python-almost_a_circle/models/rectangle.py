06d6dd458122f40b45404c6b853137c1f4ae5ce9
Dloyr <d.loyer59870@gmail.com>
2024-02-19 17:03:21 +0100

    add verifications if all value for methods setters are integers and upper to 0, or upper or equal to 0 for x and y

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -27,40 +27,115 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """"Getter method for width"""
+        """
+        Getter method for width
+
+        Returns:
+            private attribute width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """"Setter method for width"""
+        """
+        Setter method for width
+
+        Args:
+            value (int): value of width
+
+        Raises:
+            TypeError: width must be an integer
+            ValueError: width must be > 0
+        """
+        if type(value) is not int:
+            raise TypeError(f"width must be an integer")
+        if value < 1:
+            raise ValueError(f"width must be > 0")
+
         self.__width = value
 
     @property
     def height(self):
-        """Getter method for height"""
+        """
+        Getter method for height
+
+        Returns:
+            the private attribute height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter method for height"""
+        """
+        Setter method for height
+
+        Args:
+            value (int): value of height
+
+        Raises:
+            TypeError: height must be an integer
+            ValueError: height must be > 0
+        """
+        if type(value) is not int:
+            raise TypeError(f"height must be an integer")
+        if value < 1:
+            raise ValueError(f"height must be > 0")
+
         self.__height = value
 
     @property
     def x(self):
-        """Getter method for x"""
+        """
+        Getter method for x
+
+        Returns:
+            the private attribute x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setter method for x"""
+        """
+        Setter method for x
+
+        Args:
+            value (int): value of x
+
+        Raises:
+            TypeError: x must be an integer
+            ValueError: x must be >= 0
+        """
+        if type(value) is not int:
+            raise TypeError(f"x must be an integer")
+        if value < 0:
+            raise ValueError(f"x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        """Getter method for y"""
+        """
+        Getter method for y
+
+        Returns:
+            the private attribute y
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setter method for y"""
+        """
+        Setter method for y
+
+        Args:
+            value (int): value of y
+
+        Raises:
+            TypeError: y must be an integer
+            ValueError: y must be >= 0
+        """
+        if type(value) is not int:
+            raise TypeError(f"y must be an integer")
+        if value < 0:
+            raise ValueError(f"y must be >= 0")
+
         self.__y = value
