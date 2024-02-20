3be234d07141bcdd2731e1f78c060bb63da4a2ec
Dloyr <d.loyer59870@gmail.com>
2024-02-20 12:14:50 +0100

    change documentation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-
-"""class for make a square"""
+"""Module for a Square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Class for make a square"""
     def __init__(self, size, x=0, y=0, id=None):
         """
-        constructor of the class Square
+        Constructor of the class Square
 
         Args:
             size (int): the size of the square
             x (int): the width of the square
             y (int): the height of the square
-            id (int): the numbers of instance of Square
+            id (int): id of instances of Square
         """
         super().__init__(size, size, x, y, id)
         # call parameters of Rectangle with width and height = size
