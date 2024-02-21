ea417a1038ac04283b3133b158f4295b8823b9c8
Dloyr <d.loyer59870@gmail.com>
2024-02-21 09:23:08 +0100

    import json module and define a method for return the JSON string representation of a dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for Base class"""
+import json
 
 
 class Base:
@@ -18,3 +19,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """method for return the JSON string representation"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
