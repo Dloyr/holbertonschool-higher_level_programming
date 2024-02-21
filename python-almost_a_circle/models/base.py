51e1420e356cf9b62b6bc3182102ac58bce9871f
Dloyr <d.loyer59870@gmail.com>
2024-02-21 14:41:37 +0100

    class method for save the list of objects of a class in a JSON file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,8 +22,35 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """method for return the JSON string representation"""
+        """
+        Convert a list of dicitonaries to a JSON string
+
+        Args:
+            list_dictionaries (list): the list of dicitonaries
+
+        Return:
+            the JSON string
+        """
+
         if list_dictionaries is None:
             return '[]'
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Method for save the list of objects of a class in a JSON file
+
+        Args:
+            cls (class): the class
+            list_objs (list): the list of objects of the class
+        """
+        if list_objs is None:
+            list_objs = []
+
+        objects_dictionary = [objects.to_dictionary() for objects in list_objs]
+        json_string = Base.to_json_string(objects_dictionary)
+
+        with open(cls.__name__ + ".json", "w", encoding='utf-8') as file:
+            file.write(json_string)
