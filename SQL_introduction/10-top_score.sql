1b17ba57cd1f493dbc60db1ce857b83ba2cd897c
Dloyr <d.loyer59870@gmail.com>
2024-03-17 21:33:07 +0100

    organize the code on multiple lines

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,4 @@
 -- Display score and name of second_table in ordered
 
-SELECT score , name FROM second_table ORDER BY score DESC;
+SELECT score , name FROM second_table 
+ORDER BY score DESC;
--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,6 @@
 -- Display only score >= 10 and name of second_table in ordered
 
 
-SELECT score , name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score , name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
